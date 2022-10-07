11f92192ef72ae18c172fe31fdf39ac7fd1b323f
AllanOliveira2022 <115299780+AllanOliveira2022@users.noreply.github.com>
2022-10-07 16:54:55 -0300

    Resposta temporária ...

---
+++ Questão 06/Calcular_Soma_Subtracao.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main(){
+	setlocale(LC_ALL, "portuguese_brazil");
+	float n1,n2,n3,soma,subt;
+	
+	printf("CALCULAR A SOMA DE 2 NÚMEROS E DEPOIS SUBTRAIR UM TERCEIRO NÚMERO : \n");
+	printf("Digite o primeiro valor : \n ");
+	scanf("%f", &n1);
+	
+	
+	
+	
+}
