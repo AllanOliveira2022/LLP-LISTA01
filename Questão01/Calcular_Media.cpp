15e6274d4d42566532b34f20a657fc0553da9f76
AllanOliveira2022 <115299780+AllanOliveira2022@users.noreply.github.com>
2022-10-07 15:53:18 -0300

    Resposta da questão 01

---
+++ Questão01/Calcular_Media.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main(){
+	setlocale(LC_ALL, "portuguese_brazil");
+	float n1, n2, n3,media;                                                                                                                                            
+	
+	printf("CALCULAR MÉDIA DE 3 NUMEROS : \n");
+
+	printf("Digite o primeiro valor : \n");
+	scanf("%f", &n1);
+	
+	printf("Digite o segundo valor : \n");
+	scanf("%f", &n2);
+	
+	printf("Digite o terceiro valor : \n");
+	scanf("%f", &n3);
+	
+	media = (n1 + n2 + n3)/ 3;
+	
+	printf("O VALOR CALCULADO DA MÉDIA ENTRE OS NÚMEROS : %.2f , %.2f e %.2f É : %.1f  \n", n1,n2,n3,media);
+	
+	system("pause");
+	return 0;
+}
