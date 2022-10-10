18c1f5284505563b5aab24e068bab9b04a073aa6
AllanOliveira2022 <115299780+AllanOliveira2022@users.noreply.github.com>
2022-10-10 09:31:01 -0300

    Resposta da questão 06 da lista 01
    Nessa correção, eu mandei a resposta oficial da questão 06.

--- Questão 06/Calcular_Soma_Subtracao.cpp
+++ Questão 06/Calcular_Soma_Subtracao.cpp
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <locale.h>
 #include <stdlib.h>
@@ -7,8 +8,25 @@ int main(){
 	float n1,n2,n3,soma,subt;
 	
 	printf("CALCULAR A SOMA DE 2 NÚMEROS E DEPOIS SUBTRAIR UM TERCEIRO NÚMERO : \n");
-	printf("Digite o primeiro valor : \n ");
+	printf("Digite o primeiro valor a ser somado: \n ");
 	scanf("%f", &n1);
+	
+	printf("Digite o segundo valor a ser somado : \n");
+	scanf("%f", &n2);
+	
+	soma = n1 + n2;
+	
+	printf("A soma de %.2f e %.2f É :  %.2f \n",n1,n2,soma);
+	
+	printf("AGORA, DIGITE UM TERCEIRO NÚMERO PARA FAZER A SUBTRAÇÃO : \n");
+	scanf("%f", &n3);
+	
+	subt = soma - n3;
+	
+	printf("A SUBTRAÇÃO DE %.2f e %.2f É : %.2f. \n", soma,n3,subt);
+	
+	system("pause");
+	return 0;
 	
 	
 	
